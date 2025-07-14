921a7a6dfa4de5f55f28692734dd38e00d8ffd05
rawpiyaz <rawpiyaz@MacBook-Air-alikhan.local>
2025-07-14 15:11:44 +0500

    (chore): implemented decorators: log, simple_cache, validate_positive.

--- tasks/task_2.py
+++ tasks/task_2.py
@@ -1,12 +1,16 @@
-# ЗАДАНИЕ 2: Кэширование результатов
-# Напиши декоратор simple_cache, который:
-# - запоминает результат функции при вызове с конкретными аргументами,
-# - при повторном вызове с теми же аргументами — возвращает сохранённый результат без повторного вычисления,
-# - печатает "Из кэша" при использовании кэшированного значения.
-# Подсказка: используй словарь для хранения результатов.
-
 from functools import wraps
 
 
 def simple_cache(func):
     cache = {}
+
+    @wraps(func)
+    def cacher(*args, **kwargs):
+        hash = args + tuple(sorted(kwargs.items()))
+        if hash in cache:
+            print('Из кэша')
+            return cache[hash]
+        result = func(*args, **kwargs)
+        cache[hash] = result
+        return result
+    return cacher
--- tasks/task_3.py
+++ tasks/task_3.py
@@ -1,15 +1,14 @@
-# ЗАДАНИЕ 3: Проверка аргументов на положительность
-# Напиши декоратор validate_positive, который:
-# - проверяет, что все переданные числовые аргументы больше 0,
-# - если хотя бы один из них ≤ 0 — выбрасывает ValueError с сообщением "Все аргументы должны быть положительными".
-# Пример:
-# >>> @validate_positive
-# >>> def multiply(a, b): return a * b
-# >>> multiply(-1, 3)
-# ValueError: Все аргументы должны быть положительными
-
 from functools import wraps
 
 
 def validate_positive(func):
-    pass
+    @wraps(func)
+    def validator(*args, **kwargs):
+        for element in list(args) + list(kwargs.values()):
+            if (
+                isinstance(element, (int, float))
+                and element <= 0
+            ):
+                raise ValueError('Все аргументы должны быть положительными')
+        return func(*args, **kwargs)
+    return validator
